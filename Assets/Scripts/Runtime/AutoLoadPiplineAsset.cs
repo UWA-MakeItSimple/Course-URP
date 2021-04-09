278fb454473408c4e1a3952cb7dee9ee90a7ebc6
vestlee <vestleet@163.com>
2021-04-09 18:58:39 +0800

    Init

---
+++ Assets/Scripts/Runtime/AutoLoadPiplineAsset.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[ExecuteAlways]
+public class AutoLoadPiplineAsset : MonoBehaviour
+{
+    public UniversalRenderPipelineAsset _UniversalRenderPipelineAsset;
+
+    private void OnEnable()
+    {
+        UpdatePiplineAsset();
+    }
+
+    private void UpdatePiplineAsset()
+    {
+        if(_UniversalRenderPipelineAsset)
+        {
+            GraphicsSettings.renderPipelineAsset = _UniversalRenderPipelineAsset;
+        }
+    }
+}
